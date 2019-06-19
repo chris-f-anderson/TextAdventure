7f7926f6bbc53a8d365be432df0a56749646b3b2
Chris <cf_anderson@netzero.com>
2019-06-19 14:02:10 -0500

    Add project files.

---
+++ FirstProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FirstProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Birmingham");
+            Console.WriteLine("We've got a lot to offer");
+            Console.WriteLine("We have good food, entertainment, and shopping");
+            Console.WriteLine("Are you hungry?");
+            string response = Console.ReadLine();
+
+            if (response == "yes")
+            {
+                Console.WriteLine("Let me recommend BBQ.");
+                Console.WriteLine("Do you want table service?");
+                response = Console.ReadLine();
+                if (response == "yes")
+                {
+                    Console.WriteLine("Jim N' Nicks BBQ");
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Moe's BBQ");
+            }
+        }   
+    }
+}
