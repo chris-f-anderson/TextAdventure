9edfe873b0457f3890d56f818d5114a5d70fd9be
Chris <cf_anderson@netzero.com>
2019-06-19 16:23:36 -0500

    changes made at home

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -7,26 +7,28 @@ namespace FirstProject
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Birmingham");
-            Console.WriteLine("We've got a lot to offer");
-            Console.WriteLine("We have good food, entertainment, and shopping");
+            Console.WriteLine("We have great places to eat");
             Console.WriteLine("Are you hungry?");
             string response = Console.ReadLine();
 
             if (response == "yes")
             {
-                Console.WriteLine("Let me recommend BBQ.");
+                Console.WriteLine("Great, let me recommend BBQ.");
                 Console.WriteLine("Do you want table service?");
                 response = Console.ReadLine();
                 if (response == "yes")
                 {
                     Console.WriteLine("Jim N' Nicks BBQ");
-
+                
                 }
-
+                if (response == "no")
+                    Console.WriteLine("Moe's BBQ");
             }
-            else
+            if (response == "no")
+                Console.WriteLine("Too Bad, because we have great BBQ");
+            else if (response == "no")
             {
-                Console.WriteLine("Moe's BBQ");
+                
             }
         }   
     }
