584c4f7d9329157edf0d4724da2adc664e625d36
Chris <cf_anderson@netzero.com>
2019-06-20 14:17:29 -0500

    Thursday edits

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -18,18 +18,31 @@ namespace FirstProject
                 response = Console.ReadLine();
                 if (response == "yes")
                 {
-                    Console.WriteLine("Jim N' Nicks BBQ");
-                
+                    Console.WriteLine("Jim N' Nicks BBQ is my suggestion");
+                    Console.WriteLine("Are you ready to order?");
+                    response = Console.ReadLine();
+                    if (response == "yes")
+                    {
+                        Console.WriteLine("Meal 1, 2, or 3?");
+                        response = Console.ReadLine();
+                        if (response == "1")
+                            Console.WriteLine("You're getting a pulled pork sandwich.");
+                        else if (response == "2")
+                            Console.WriteLine("You're getting the brisket plate.");
+                        else if (response == "3")
+                            Console.WriteLine("You're getting a rib plate.");
+                        else
+                            Console.WriteLine("Well you're not going to eat then.");
+                    }
+
                 }
-                if (response == "no")
+                else if (response == "no")
                     Console.WriteLine("Moe's BBQ");
             }
-            if (response == "no")
-                Console.WriteLine("Too Bad, because we have great BBQ");
             else if (response == "no")
-            {
-                
-            }
+                Console.WriteLine("Too Bad, because we have great BBQ");
+            else
+                Console.WriteLine("You're wasting my time!");
         }   
     }
 }
